039d7363004c62b1b1f2d71dfbf927ce07f02d7b
Ruben Alvarez <ralvarezmar@gmail.com>
2019-02-20 17:21:56 +0100

    añadido gitignore

--- git.sh
+++ git.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-if [ $# -ne 1 ]
+if [ $# -gt 2 ]
 then
 	echo "usage: $1 " 1>&2
 	exit 1
 fi
 git add .
 git commit -m $1
-git push master
-git push upstream
+git push 
+if [ $2 = "-t" ]
+then
+	git push upstream
+fi
