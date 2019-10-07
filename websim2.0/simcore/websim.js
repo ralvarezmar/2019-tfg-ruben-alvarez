7a8cf34140dca6942bc8226662d4c135a9481093
Rubén Álvarez <ralvarezmar@gmail.com>
2019-10-07 17:29:29 +0200

    testing camera issue

---
+++ websim2.0/simcore/websim.js
@@ -0,0 +1,46 @@
+import 'aframe';
+import 'aframe-extras';
+import 'aframe-physics-system';
+import $ from 'jquery';
+import * as robots from './robots';
+import * as simulation from './simulation';
+import * as config from './config';
+import * as utils from './utils';
+import * as globals from './globals';
+
+window.$ = $;
+
+var Websim = {
+  globals: globals,
+  utils: utils,
+  simulation: simulation,
+  robots: robots,
+  config: config
+};
+
+Websim.reservedVariables = ['myRobot,', 'mainInterval,', 'myRobot;', 'mainInterval;'];
+
+
+Websim.config.init = async (configFile)=>{
+  /**
+   * This function is used to configure Websim, some
+   * functions needs to be executed before anything due to
+   * they needs to listen events or extend AFrame components
+   *
+   * @param {string} configFile Config file url to request to load scene and robots
+   */
+  return new Promise(async (resolve, reject) =>{
+    Websim.simulation.robotLoader();
+    Websim.simulation.extendAFrame();
+    await Websim.config.loadJSON(configFile);
+    Websim.config.activateMainCamera();
+    await Websim.robots.createRobots();
+    Websim.robots.evaluator();
+    Websim.simulation.changeCamera();
+    resolve();
+  });
+}
+
+
+// This line exports websim to be used from other scripts
+module.exports = window.Websim = Websim;
