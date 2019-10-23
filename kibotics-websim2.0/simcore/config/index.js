4fb01d4d25e67444c639fead27659202d4b53e33
Rubén Álvarez <ralvarezmar@gmail.com>
2019-10-23 11:01:12 +0200

    bridge in competitive

--- kibotics-websim2.0/simcore/config/index.js
+++
@@ -1,2 +0,0 @@
-export {loadJSON, parser} from './config_parser.js';
-export * from './cameras.js';
--- kibotics-websim2.0/simcore/globals/variables.js
+++
@@ -1,8 +0,0 @@
-export var arrayIds = [];
-export var arrayRobots = [];
-export var arrayLoadedBodyRobots = [];
-export var simEnabled = true;
-
-export function changeSimulationValue(value){
-    simEnabled = value;
-}
