7d72c9ab16dbb57e8e1318990b802e85e5b3f4bc
Rubén Álvarez <ralvarezmar@gmail.com>
2019-10-24 11:04:13 +0200

    evaluators

--- kibotics-websim2.0/teleoperators/js/teleop.js
+++ kibotics-websim2.0/teleoperators/js/teleop.js
@@ -8,21 +8,21 @@ $(document).ready(()=>{
       toggleCameraDisplay();
     });
 
-    $("#spectatorCamera").click(()=>{
-      var subjCamera = document.querySelector("#subjCamera");
-      var spectatorCamera = document.querySelector("#primaryCamera");
-      var firstPersonCamera = document.querySelector("#firstPersonCamera");
-      var camera1 = subjCamera.getAttribute('camera','active');
-      var camera2 = spectatorCamera.getAttribute('camera','active');
-      var camera3 = firstPersonCamera.getAttribute('camera','active');
-      if(camera1.active===true){
-        spectatorCamera.setAttribute('camera', 'active', true);
-      }else if(camera2.active===true){
-        firstPersonCamera.setAttribute('camera', 'active', true);
-      }else if(camera3.active==true){
-        subjCamera.setAttribute('camera', 'active', true);
-      }
-    });
+    // $("#spectatorCamera").click(()=>{
+    //   var subjCamera = document.querySelector("#subjCamera");
+    //   var spectatorCamera = document.querySelector("#primaryCamera");
+    //   var firstPersonCamera = document.querySelector("#firstPersonCamera");
+    //   var camera1 = subjCamera.getAttribute('camera','active');
+    //   var camera2 = spectatorCamera.getAttribute('camera','active');
+    //   var camera3 = firstPersonCamera.getAttribute('camera','active');
+    //   if(camera1.active===true){
+    //     spectatorCamera.setAttribute('camera', 'active', true);
+    //   }else if(camera2.active===true){
+    //     firstPersonCamera.setAttribute('camera', 'active', true);
+    //   }else if(camera3.active==true){
+    //     subjCamera.setAttribute('camera', 'active', true);
+    //   }
+    // });
 
   document.addEventListener('robot-loaded', (evt)=>{
     localRobot = evt.detail;
